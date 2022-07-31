2bc357caefe624e1be2357f51ec9c88e53a8f77b
Mark Lauron <malauron@gmail.com>
2022-08-01 06:20:36 +0800

    Incremental updates

--- src/main/java/com/toolkit/inventory/Repository/CartMenuRepository.java
+++ src/main/java/com/toolkit/inventory/Repository/CartMenuRepository.java
@@ -3,9 +3,14 @@ package com.toolkit.inventory.Repository;
 import com.toolkit.inventory.Domain.CartMenu;
 import com.toolkit.inventory.Projection.CartMenuView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(excerptProjection = CartMenuView.class)
 public interface CartMenuRepository extends JpaRepository<CartMenu, Long> {
 
+    @Query(value = "SELECT * FROM cart_menus order by cart_menu_id", nativeQuery = true)
+    List<CartMenu> findAllCartMenus();
 }
