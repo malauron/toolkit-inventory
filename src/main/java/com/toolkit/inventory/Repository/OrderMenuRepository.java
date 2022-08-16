06cd6028a0698d1955665894c06e1f291f40ef46
Mark Lauron <malauron@gmail.com>
2022-08-16 18:52:07 +0800

    Incremental udpates

--- src/main/java/com/toolkit/inventory/Repository/OrderMenuRepository.java
+++ src/main/java/com/toolkit/inventory/Repository/OrderMenuRepository.java
@@ -2,11 +2,13 @@ package com.toolkit.inventory.Repository;
 
 import com.toolkit.inventory.Domain.Order;
 import com.toolkit.inventory.Domain.OrderMenu;
+import com.toolkit.inventory.Projection.OrderMenuView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Set;
 
-//@RepositoryRestResource(excerptProjection = OrderMenuView.class)
+@RepositoryRestResource(excerptProjection = OrderMenuView.class)
 public interface OrderMenuRepository extends JpaRepository<OrderMenu, Long> {
     Set<OrderMenu> findAllByOrderOrderByOrderMenuId(Order order);
 }
