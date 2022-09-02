556cb13aa390f4a8ab322d4ee0525ee5f5956269
Mark Lauron <malauron@gmail.com>
2022-09-02 18:54:50 +0800

    Incremental updates - Customers module

--- src/main/java/com/toolkit/inventory/Domain/Customer.java
+++ src/main/java/com/toolkit/inventory/Domain/Customer.java
@@ -1,9 +1,6 @@
 package com.toolkit.inventory.Domain;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonIncludeProperties;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.fasterxml.jackson.annotation.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -34,6 +31,34 @@ public class Customer {
 
     @Column(name = "address")
     private String address;
+
+    @JsonManagedReference
+    @OneToOne(mappedBy = "customer")
+    private CustomerPicture customerPicture;
+
+    @Column(name = "blood_type")
+    private String bloodType;
+
+    @Column(name = "sss_no")
+    private String sssNo;
+
+    @Column(name = "hdmf_no")
+    private String hdmfNo;
+
+    @Column(name = "phic_no")
+    private String phicNo;
+
+    @Column(name = "tin_no")
+    private String tinNo;
+
+    @Column(name = "er_contact_person")
+    private String erContactPerson;
+
+    @Column(name = "er_contact_address")
+    private String erContactAddress;
+
+    @Column(name = "er_contact_no")
+    private String erContactNo;
 
     @CreationTimestamp
     @Column(name = "date_created")
---
+++ src/main/java/com/toolkit/inventory/Domain/CustomerPicture.java
@@ -0,0 +1,32 @@
+package com.toolkit.inventory.Domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "customer_pictures")
+public class CustomerPicture {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "customer_picture_id")
+  private Long customerPictureId;
+
+  @JsonBackReference
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "customer_id",referencedColumnName = "customer_id")
+  private Customer customer;
+
+  @Lob
+  @Column(name = "file")
+  private byte file;
+
+  @Column(name = "type")
+  private String type;
+
+}
