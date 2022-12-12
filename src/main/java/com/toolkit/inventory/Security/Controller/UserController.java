338e7fa98c061c1b9db1d4d6ea6a176a7a668992
Mark Lauron <malauron@gmail.com>
2022-12-13 06:54:50 +0800

    Incremental Updates

--- src/main/java/com/toolkit/inventory/Security/Controller/UserController.java
+++ src/main/java/com/toolkit/inventory/Security/Controller/UserController.java
@@ -1,8 +1,16 @@
 package com.toolkit.inventory.Security.Controller;
 
+import com.toolkit.inventory.Security.Domain.User;
 import com.toolkit.inventory.Security.Dto.UserDto;
 import com.toolkit.inventory.Security.Service.UserService;
+import com.toolkit.inventory.Security.Utility.JWTTokenProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @CrossOrigin
 @RestController
@@ -10,9 +18,22 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private UserService userService;
+    private AuthenticationManager authenticationManager;
+    private JWTTokenProvider jwtTokenProvider;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService,
+                          JWTTokenProvider jwtTokenProvider) {
         this.userService = userService;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        User loginUser = userService.(user.getUsername());
+        UserPrincipal userPrincipal = new UserPrincipal(loginUser);
+        HttpHeaders jwtHeader = getJwtHeader(userPrincipal);
+        return new ResponseEntity<>(loginUser, jwtHeader, OK);
     }
 
     @PutMapping("/users")
