c11a62c61730e6a805663222bb87e29374551130
Mark Lauron <malauron@gmail.com>
2023-05-30 17:39:50 +0800

    Incremental Updates

--- src/main/java/com/toolkit/inventory/Repository/PosItemPriceLevelRepository.java
+++ src/main/java/com/toolkit/inventory/Repository/PosItemPriceLevelRepository.java
@@ -3,8 +3,21 @@ package com.toolkit.inventory.Repository;
 import com.toolkit.inventory.Domain.PosItemPriceLevel;
 import com.toolkit.inventory.Projection.PosItemPriceLevelView;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @RepositoryRestResource(excerptProjection = PosItemPriceLevelView.class)
 public interface PosItemPriceLevelRepository extends JpaRepository<PosItemPriceLevel, Long> {
+
+    @Query(value = "SELECT p FROM PosItemPriceLevel p " +
+            "WHERE p.posItemPrice.posItemPriceId = :posItemPriceId " +
+            "AND p.customerGroup.customerGroupId = :customerGroupId")
+    Optional<PosItemPriceLevel> findByPosItemPriceIdAndCustomerGroupId(
+            @RequestParam Long posItemPriceId,
+            @RequestParam Long customerGroupId
+    );
+
 }
--- src/main/java/com/toolkit/inventory/Service/PosItemPriceServiceImp.java
+++ src/main/java/com/toolkit/inventory/Service/PosItemPriceServiceImp.java
@@ -1,79 +1,147 @@
 package com.toolkit.inventory.Service;
 
-import com.toolkit.inventory.Domain.Item;
-import com.toolkit.inventory.Domain.PosItemPrice;
-import com.toolkit.inventory.Domain.Warehouse;
+import com.toolkit.inventory.Domain.*;
 import com.toolkit.inventory.Dto.PosItemPriceDto;
 import com.toolkit.inventory.Projection.PosItemPriceView;
-import com.toolkit.inventory.Repository.ItemRepository;
-import com.toolkit.inventory.Repository.PosItemPriceRepository;
-import com.toolkit.inventory.Repository.WarehouseRepository;
+import com.toolkit.inventory.Repository.*;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class PosItemPriceServiceImp implements PosItemPriceService {
 
     private PosItemPriceRepository posItemPriceRepository;
+    private PosItemPriceLevelRepository posItemPriceLevelRepository;
     private ItemRepository itemRepository;
     private WarehouseRepository warehouseRepository;
+    private CustomerGroupRepository customerGroupRepository;
 
     public PosItemPriceServiceImp(
             PosItemPriceRepository posItemPriceRepository,
+            PosItemPriceLevelRepository posItemPriceLevelRepository,
             ItemRepository itemRepository,
-            WarehouseRepository warehouseRepository
+            WarehouseRepository warehouseRepository,
+            CustomerGroupRepository customerGroupRepository
     ){
         this.posItemPriceRepository = posItemPriceRepository;
+        this.posItemPriceLevelRepository = posItemPriceLevelRepository;
         this.itemRepository = itemRepository;
         this.warehouseRepository = warehouseRepository;
+        this.customerGroupRepository = customerGroupRepository;
     }
 
     @Override
     public PosItemPriceDto save(PosItemPriceDto posItemPriceDto) {
 
-        Optional<Item> optionalItem =  this.itemRepository
-                .findById(posItemPriceDto.getItem().getItemId());
-        posItemPriceDto.setItem(optionalItem.get());
-
-        Optional<Warehouse> optionalWarehouse = this.warehouseRepository
-                .findByWarehouseId(posItemPriceDto.getWarehouse().getWarehouseId());
-        posItemPriceDto.setWarehouse(optionalWarehouse.get());
+        PosItemPrice posItemPrice;
 
         if (posItemPriceDto.getPosItemPriceId() > 0) {
 
+            Optional<PosItemPrice> optionalPosItemPrice = this.posItemPriceRepository
+                                                            .findById(posItemPriceDto.getPosItemPriceId());
+            posItemPrice = optionalPosItemPrice.get();
+            posItemPrice.setDefaultPrice(posItemPriceDto.getDefaultPrice());
+
+            this.posItemPriceRepository.save(posItemPrice);
+
         } else {
 
-            PosItemPrice posItemPrice = new PosItemPrice();
+            posItemPrice = new PosItemPrice();
+
+            Optional<Item> optionalItem =  this.itemRepository
+                    .findById(posItemPriceDto.getItem().getItemId());
             posItemPrice.setItem(optionalItem.get());
+
+            Optional<Warehouse> optionalWarehouse = this.warehouseRepository
+                    .findByWarehouseId(posItemPriceDto.getWarehouse().getWarehouseId());
             posItemPrice.setWarehouse(optionalWarehouse.get());
+
             posItemPrice.setDefaultPrice(posItemPriceDto.getDefaultPrice());
 
             try {
 
-                this.savePosItemPrice(posItemPrice);
-
-                posItemPriceDto.setPosItemPriceId(posItemPrice.getPosItemPriceId());
+                this.posItemPriceRepository.save(posItemPrice);
 
             } catch (DataIntegrityViolationException e){
+
                 Optional<PosItemPriceView> optionalPosItemPrice = this.posItemPriceRepository
-                                                                .findByWarehouseIdAndItemId(
+                                                                        .findByWarehouseIdAndItemId(
                                                                         posItemPrice.getWarehouse().getWarehouseId(),
                                                                         posItemPrice.getItem().getItemId());
-
                 posItemPrice.setPosItemPriceId(optionalPosItemPrice.get().getPosItemPriceId());
 
-                this.savePosItemPrice(posItemPrice);
-
-                posItemPriceDto.setPosItemPriceId(posItemPrice.getPosItemPriceId());
+                this.posItemPriceRepository.save(posItemPrice);
 
             };
+
+            posItemPriceDto.setPosItemPriceId(posItemPrice.getPosItemPriceId());
+
         }
+
+        posItemPriceDto.setItem(posItemPrice.getItem());
+        posItemPriceDto.setWarehouse(posItemPrice.getWarehouse());
+
+        Set<PosItemPriceLevel> posItemPriceLevels = new HashSet<>();
+
+        posItemPriceDto.getPosItemPriceLevels().forEach(priceLevelDto -> {
+
+            PosItemPriceLevel posItemPriceLevel;
+
+            if (priceLevelDto.getPosItemPriceLevelId() > 0) {
+
+                Optional<PosItemPriceLevel> optPriceLevel = this.posItemPriceLevelRepository
+                                                                    .findById(priceLevelDto.getPosItemPriceLevelId());
+
+                posItemPriceLevel = optPriceLevel.get();
+                posItemPriceLevel.setPrice(priceLevelDto.getPrice());
+
+                this.posItemPriceLevelRepository.save(posItemPriceLevel);
+
+            } else {
+
+                posItemPriceLevel = new PosItemPriceLevel();
+                posItemPriceLevel.setPosItemPrice(posItemPrice);
+
+                Optional<CustomerGroup> optCustGrp = this.customerGroupRepository
+                                                            .findById(priceLevelDto.getCustomerGroup().getCustomerGroupId());
+                posItemPriceLevel.setCustomerGroup(optCustGrp.get());
+                posItemPriceLevel.setPrice(priceLevelDto.getPrice());
+
+                System.out.println(posItemPriceLevel);
+
+                try {
+
+                    this.posItemPriceLevelRepository.save(posItemPriceLevel);
+
+                } catch (DataIntegrityViolationException e) {
+
+                    Optional<PosItemPriceLevel> optPriceLevel = this.posItemPriceLevelRepository
+                                                                        .findByPosItemPriceIdAndCustomerGroupId(
+                                                                                posItemPriceLevel.getPosItemPrice().getPosItemPriceId(),
+                                                                                posItemPriceLevel.getCustomerGroup().getCustomerGroupId()
+                                                                        );
+                    posItemPriceLevel.setPosItemPriceLevelId(optPriceLevel.get().getPosItemPriceLevelId());
+
+                    this.posItemPriceLevelRepository.save(posItemPriceLevel);
+
+                }
+
+            }
+
+            posItemPriceLevels.add(posItemPriceLevel);
+
+//            posItemPriceDto.getPosItemPriceLevels().add(posItemPriceLevel);
+
+        });
+
+        posItemPriceDto.setPosItemPriceLevels(posItemPriceLevels);
+
         return posItemPriceDto;
     }
 
-    private void savePosItemPrice(PosItemPrice posItemPrice) throws DataIntegrityViolationException {
-        this.posItemPriceRepository.saveAndFlush(posItemPrice);
-    }
 }
