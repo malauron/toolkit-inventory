43646102c56728ddeb6a56450176adebaaaf6882
Mark Lauron <malauron@gmail.com>
2023-06-27 18:58:18 +0800

    Incremental Updates

--- src/main/java/com/toolkit/inventory/Repository/ItemUomRepository.java
+++ src/main/java/com/toolkit/inventory/Repository/ItemUomRepository.java
@@ -3,24 +3,29 @@ package com.toolkit.inventory.Repository;
 import com.toolkit.inventory.Domain.Item;
 import com.toolkit.inventory.Domain.ItemUom;
 import com.toolkit.inventory.Domain.ItemUomId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Set;
 
 @RepositoryRestResource
 public interface ItemUomRepository extends JpaRepository<ItemUom, ItemUomId> {
 
-//  @Modifying
-//  @Query("UPDATE ItemUom x SET x.quantity = :quantity WHERE x.itemUomId=:itemUomId")
-//  void setQuantity(@Param("itemUomId") ItemUomId itemUomId, @Param("quantity") BigDecimal quantity);
-
-//  @Query(value = "SELECT i FROM ItemUom i WHERE i.itemUomId=:itemUomId")
-//  Set<ItemUom> findByItemUomId(ItemUomId itemUomId);
+  @Query(value = "SELECT i FROM ItemUom i WHERE i.item.itemId = :itemId AND i.uom.uomName LIKE %:uomName% " +
+                 "ORDER BY i.uom.uomName")
+  Page<ItemUom> findByItemIdUomName(
+          @RequestParam("itemId") Long itemId,
+          @RequestParam("uomName") String uomName,
+          Pageable pageable);
 
   @Query(value = "SELECT i FROM ItemUom i WHERE i.item=:item")
   Set<ItemUom> findByItem(Item item);
+
+
 
 }
