910120adb17a7e297122d1708f9df784eb23fdc6
Mark Lauron <malauron@gmail.com>
2024-05-03 19:08:18 +0800

    Incremental Updates

--- src/main/java/com/toolkit/inventory/Repository/ProjectPaymentRepository.java
+++ src/main/java/com/toolkit/inventory/Repository/ProjectPaymentRepository.java
@@ -1,11 +1,23 @@
 package com.toolkit.inventory.Repository;
 
 import com.toolkit.inventory.Domain.ProjectPayment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RepositoryRestResource()
 public interface ProjectPaymentRepository extends JpaRepository<ProjectPayment, Long> {
 
-
+    @Query(value = "SELECT p FROM ProjectPayment p " +
+                   "WHERE CONCAT(p.paymentId, '') LIKE %:searchDesc% " +
+                   "OR p.client.clientName LIKE %:searchDesc% " +
+                   "OR p.referenceNo LIKE %:searchDesc% " +
+                   "ORDER BY p.paymentId DESC")
+    Page<ProjectPayment> findByCustomParam(
+            @RequestParam("searchDesc") String searchDesc,
+            Pageable pageable
+    );
 }
