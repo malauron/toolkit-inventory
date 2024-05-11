40e6ddf471fd24ecd146736dbe7a8eb2ee14e51e
Mark Lauron <malauron@gmail.com>
2024-05-11 14:21:23 +0800

    Incremental Updates

--- src/main/java/com/toolkit/inventory/Repository/ProjectContractRepository.java
+++ src/main/java/com/toolkit/inventory/Repository/ProjectContractRepository.java
@@ -2,9 +2,17 @@ package com.toolkit.inventory.Repository;
 
 import com.toolkit.inventory.Domain.ProjectContract;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Set;
 
 @RepositoryRestResource
 public interface ProjectContractRepository extends JpaRepository<ProjectContract, Long> {
 
+    @Query(value = "SELECT p FROM ProjectContract p " +
+                    "WHERE p.isCancelled = false " +
+                    "AND p.ttlBalance > 0 AND p.client.clientId = :clientId " +
+                    "ORDER BY p.contractId")
+    Set<ProjectContract> getActiveContractsByClientId(Long clientId);
 }
